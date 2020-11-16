943eb2ad774cf2eba2a5bacd8a10c3daa3b87988
Dan <dan.bertrand.hs@gmail.com>
2020-11-16 17:45:27 +0100

    Nearly done

--- script.js
+++ script.js
@@ -95,3 +95,120 @@ navBar.addEventListener("dblclick", function removeBootstrap(){
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //// Functionality 6
 
+for (var i = 0; i < 6; i++) {
+
+let btnView = document.getElementsByClassName("btn-success")[i];
+let cardViewStatus = false;
+let cardText = document.getElementsByClassName("card-text")[i];
+let cardImage = document.getElementsByClassName("card-img-top")[i];
+
+btnView.addEventListener("mouseover", function editCard(){
+
+	if (cardViewStatus == false) {
+		cardText.style.display = "none";
+		cardImage.style.width = "20%";
+		cardImage.style.height = "20%";
+		cardViewStatus = true;
+	}
+	else if (cardViewStatus == true) {
+		cardText.style.display = "block";
+		cardImage.style.width = "100%";
+		cardImage.style.height = "100%";
+		cardViewStatus = false;
+	}
+})};
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//// Functionality 7
+
+
+
+let arrow = document.querySelectorAll("section.jumbotron")[0].getElementsByTagName("a")[1];
+
+arrow.addEventListener("click", function cardsRotation(){
+	let lastCard = document.getElementsByClassName("col-md-4")[5];
+	let parent = document.getElementsByClassName("row")[1];
+	parent.insertBefore(lastCard, parent.childNodes[0]);
+} )
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//// Functionality 8
+
+
+
+
+let arrowBack = document.querySelectorAll("section.jumbotron")[0].getElementsByTagName("a")[0];
+
+arrowBack.addEventListener("click", function cardsRotationInverse(event){
+	event.preventDefault();
+	let firstCard = document.getElementsByClassName("col-md-4")[0];
+	let parent = document.getElementsByClassName("row")[1];
+	parent.appendChild(firstCard);
+} );
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//// Functionality 9
+
+
+let logo = document.getElementsByTagName("a")[3];
+let body = document.getElementsByTagName("body")[0];
+let logoStatus = false;
+
+
+logo.addEventListener("click", function selectLogo(){
+	
+
+	if (logoStatus == false) {
+		logoStatus = true;
+		logo.style.backgroundColor = "green";
+		if (logoStatus == true) {
+			logo.addEventListener("keydown", function (event) {
+
+			  switch (event.key) {
+			    case "ArrowDown":
+			      body.style.backgroundColor = "orange";
+			      logoStatus == false; 
+			      break;
+			    case "ArrowLeft":
+			      body.style.backgroundColor = "grey";
+			      logoStatus == false; 
+			      break;
+			    case "Enter":
+			      body.style.backgroundColor = "yellow";
+			      logoStatus == false; 
+			      break;
+			    default:
+			      logoStatus == false; // Quitter lorsque cela ne gère pas l'événement touche.
+			  }
+
+			});
+
+		}
+
+
+	}
+
+
+
+	else if (logoStatus == true) {
+		logoStatus = false;
+		logo.style.backgroundColor = "red";
+	}
+});
+
+
+
